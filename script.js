7ad1a31fc29f78235074dea96dcca158d9492108
Sadig <sadigalipasheyev@gmail.com>
2022-11-22 01:09:03 +0400

    js updated

--- script.js
+++ script.js
@@ -20,38 +20,45 @@ function calculationForCurrency(sum1,i,sum_num1,sum_num2){
     currency[i] = sum1 * data.rates[sum_num2];
     })
 }
-changeValute();
 document.querySelectorAll('.left').forEach((item,index)=>{
     item.addEventListener('click',()=>{
         item.style.backgroundColor = '#833AE0';
+        item.style.color = 'white';
     for(let i = 0; i<document.querySelectorAll('.left').length;i++){
         if(i!=index){
-        document.querySelectorAll('.left')[i].style.backgroundColor = 'white'
+        document.querySelectorAll('.left')[i].style.backgroundColor = 'white';
+        document.querySelectorAll('.left')[i].style.color = '#C6C6C6';
         }
     }
     valute[0] = item.innerHTML;
-    changeValute();
     calculation(document.querySelectorAll('input')[1].value,document.querySelectorAll('input')[0],valute[1],valute[0]);
     })
 })
 document.querySelectorAll('.right').forEach((item,index)=>{
     item.addEventListener('click',()=>{
         item.style.backgroundColor = '#833AE0';
+        item.style.color = 'white';
         for(let i = 0; i<document.querySelectorAll('.right').length;i++){
             if(i!=index){
+            document.querySelectorAll('.right')[i].style.color = '#C6C6C6';
             document.querySelectorAll('.right')[i].style.backgroundColor = 'white'
             }
         }
     valute[1] = item.innerHTML;
-    changeValute();
     calculation(document.querySelectorAll('input')[0].value,document.querySelectorAll('input')[1],valute[0],valute[1]);
     })
 })
+document.querySelectorAll('th').forEach((item)=>{
+  item.addEventListener('click',()=>{
+    changeValute();
+  })
+})
 document.querySelectorAll('input')[0].addEventListener('input', (event) => {
 calculation(document.querySelectorAll('input')[0].value,document.querySelectorAll('input')[1],valute[0],valute[1]);
+changeValute();
 })
-
 document.querySelectorAll('input')[1].addEventListener('input', (event) => {
 calculation(document.querySelectorAll('input')[1].value,document.querySelectorAll('input')[0],valute[1],valute[0]);
+changeValute();
 })
     
