f8e9ca5457abdaa92e9039c219c589dc76c405a3
Sadig <sadigalipasheyev@gmail.com>
2022-11-26 00:04:54 +0400

    js updated

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 let e = 0;
+let answer;
 let valute = ['RUB','USD'];
+function space (num){
+    let dot = false;
+    for(let i = 0; i<num.length;i++)
+        if(num[i] == ".") dot = true;
+    if(dot){
+        let parts = num.toString().split(".");
+        for(let i = 0;i<2;i++)
+            parts[i] = parts[i].replace(/\D/g,'');
+        parts [0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        return parts.join(".");
+    }
+    else{
+        let parts = num.toString();
+        parts = parts.replace(/\D/g,'');
+        parts = parts.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        return parts;
+    }
+}
+function despace(num){
+    let dot = false;
+    for(let i = 0; i<num.length;i++)
+        if(num[i] == ".") dot = true;
+    if(dot){
+        num = num.split(".")
+        num[0] = num[0].replace(/\D/g,'');
+        return num.join(".")
+    }
+    else{
+    return num = num.replace(/\D/g,'');
+    }
+}
 function calculation(sum1,sum2,sum_num1,sum_num2){
     fetch(`https://api.exchangerate.host/latest?base=${sum_num1}&symbols=${sum_num2}`)
     .then(response => response.json())
     .then(data => {
+    sum1 = despace(sum1);
     sum2.value = sum1 * data.rates[sum_num2];
-    return up(sum2);
+    sum2.value = space(sum2.value);
     })
     .catch((err)=>{
         alert('Connection problem');
@@ -13,39 +46,23 @@ function calculation(sum1,sum2,sum_num1,sum_num2){
 }
 function changeValute(){
     for(let i = 0; i<2;i++){
-    if(i==0){
-    fetch(`https://api.exchangerate.host/latest?base=${valute[0]}&symbols=${valute[1]}`)
+    fetch(`https://api.exchangerate.host/latest?base=${valute[i]}&symbols=${valute[i==0?1:0]}`)
     .then(response => response.json())
     .then(data => {
-        document.querySelectorAll('.ammount_p')[0].innerHTML = `1 ${valute[0]} = ${data.rates[valute[1]]} ${valute[1]}`
+        document.querySelectorAll('.ammount_p')[i].innerHTML = `1 ${valute[i]} = ${data.rates[valute[i==0?1:0]]} ${valute[i==0?1:0]}`
     })
     .catch((err)=>{
         alert('Connection problem');
     })
-}
-   else{
-    fetch(`https://api.exchangerate.host/latest?base=${valute[1]}&symbols=${valute[0]}`)
-    .then(response => response.json())
-    .then(data => {
-        document.querySelectorAll('.ammount_p')[1].innerHTML = `1 ${valute[1]} = ${data.rates[valute[0]]} ${valute[0]}`
-    })
-    .catch((err)=>{
-        alert('Connection problem');
-    })
-   }
 }
 }
 changeValute();
 function up(e) {
+    e.value = space(e.value);
     if (e.value.indexOf(".") != '-1') {
       e.value=e.value.substring(0, e.value.indexOf(".") + 5);
     }
 }
-// function space(e){
-//     let parts = e.toString().split(".");
-//     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");                     
-//     return parts.join(".");
-// }
 document.querySelectorAll('.left').forEach((item,index)=>{
     item.addEventListener('click',()=>{
         item.style.backgroundColor = '#833AE0';
@@ -59,6 +76,7 @@ document.querySelectorAll('.left').forEach((item,index)=>{
     valute[0] = item.innerHTML;
     changeValute();
     calculation(document.querySelectorAll('input')[1].value,document.querySelectorAll('input')[0],valute[1],valute[0]);
+    console.log(answer);
     })
 })
 document.querySelectorAll('.right').forEach((item,index)=>{
@@ -79,6 +97,7 @@ document.querySelectorAll('.right').forEach((item,index)=>{
 document.querySelectorAll('input')[0].addEventListener('input', (event) => {
 calculation(document.querySelectorAll('input')[0].value,document.querySelectorAll('input')[1],valute[0],valute[1]);
 changeValute();
+console.log(answer);
 })
 document.querySelectorAll('input')[1].addEventListener('input', (event) => {
 calculation(document.querySelectorAll('input')[1].value,document.querySelectorAll('input')[0],valute[1],valute[0]);
